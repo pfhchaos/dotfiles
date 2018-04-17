a7487bb596325eae9b83871747a1cbafae423dde
pfhchaos <pfh.chaos@gmail.com>
2018-04-16 17:05:44 -0700

    as;ldk

--- .config/i3/switch-workspace.py
+++ .config/i3/switch-workspace.py
@@ -25,16 +25,11 @@ else:
     workspace_new = argv[1]
 
 
-
 for i in list(filter(lambda o: o.focused, i3.get_workspaces())):
     active = i
-#    active_workspace = i.name
-#    active_display = i.output
-#    active_visible = i.visible
-#    active_x = i.rect.x
-#    active_y = i.rect.y
 
-for i in list(filter(lambda o: o.num == int(workspace_new), i3.get_workspaces())):
+new = active
+for i in list(filter(lambda o: int(o.num) == int(workspace_new), i3.get_workspaces())):
     new = i
 
 print("active_workspace is " + active.name)
@@ -42,16 +37,17 @@ print("active_display is " + active.output)
 print("active_visible is " + str(active.visible))
 print("active_x is " + str(active.rect.x))
 print("active_y is " + str(active.rect.y))
-print("new_workspace is " + new.name)
-print("new_display is " + new.output)
-print("new_visible is " + str(new.visible))
-print("new_x is " + str(new.rect.x))
-print("new_y is " + str(new.rect.y))
+#print("new_workspace is " + new.name)
+#print("new_display is " + new.output)
+#print("new_visible is " + str(new.visible))
+#print("new_x is " + str(new.rect.x))
+#print("new_y is " + str(new.rect.y))
 
-mouse_offset_x = mouse_location.x - active.rect.x + new.rect.x
-mouse_offset_y = mouse_location.y - active.rect.y + new.rect.y
-print("mouse_offset_x is " + str(mouse_location.x))
-print("mouse_offset_y is " + str(mouse_location.y))
+if (new != None):
+    mouse_offset_x = mouse_location.x - active.rect.x + new.rect.x
+    mouse_offset_y = mouse_location.y - active.rect.y + new.rect.y
+    print("mouse_offset_x is " + str(mouse_location.x))
+    print("mouse_offset_y is " + str(mouse_location.y))
 
 # if the 2 workspaces are already on the same output just switch
 if (active.output == new.output):
